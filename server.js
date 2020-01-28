5250b34b5d73f5f07ad1b05edd615554dcdcf4a2
Leah Migacz <lmigacz@msudenver.edu>
2020-01-27 21:12:41 -0700

    added server connection

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "greenp0ison",
+    database: "employeeDB"
+  });
+
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log(`Server connected as id ${connection.threadId} \n`);
+  });
+
+  
