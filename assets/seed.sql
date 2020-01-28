d5a22eefd33795f5652ee19813c8c0c74b7ef09b
Leah Migacz <lmigacz@msudenver.edu>
2020-01-27 23:02:15 -0700

    added primary key to database

--- assets/seed.sql
+++ assets/seed.sql
@@ -13,7 +13,8 @@ CREATE TABLE position (
 id INT NOT NULL AUTO_INCREMENT,
 title VARCHAR(30) NOT NULL,
 salary VARCHAR(20) NOT NULL,
-department_id INT
+department_id INT,
+PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
@@ -21,7 +22,8 @@ id INT NOT NULL AUTO_INCREMENT,
 first_name VARCHAR(30) NOT NULL,
 last_name VARCHAR(30) NOT NULL,
 role_id INT,
-manager_id INT
+manager_id INT,
+PRIMARY KEY (id)
 );
 
 INSERT INTO department (name)
